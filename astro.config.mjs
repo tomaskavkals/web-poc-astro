c46ef379296ba88d8a784cd3d4a77576085d5833
Tomáš Kavka <tomas.kavka@livesport.eu>
2024-07-08 09:59:39 +0200

    Render on edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  output: "hybrid",
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
   integrations: [react(), tailwind()],
 });
